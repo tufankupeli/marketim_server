b3b0aa22195efd684a02ddee954ba3de6f52ae5d
Tufan Küpeli <kupelitufam@gmail.com>
2017-12-04 19:51:52 +0300

    delete old files

--- src/presentation/MarketimWebApi/Base/OperationsCodeEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace MarketimWebApi.Base
-{
-    public enum OperationsCodeEnum:int
-    {
-        Undefined=0,
-        GetCustomerInfo=1
-    }
-}
--- src/presentation/MarketimWebApi/Base/SessionConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace MarketimWebApi.Base
-{
-    public class SessionConstants
-    {
-        public static readonly string SC_CUSTOMER = "sk_customer";
-    }
-}
